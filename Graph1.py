2dc3be0e9f7b5e850a7199477db07ccb596579ac
MakeRollMake <50707253+MakeRollMake@users.noreply.github.com>
2023-01-08 17:55:56 +0100

    Update Graph1.py

--- Graph1.py
+++ Graph1.py
@@ -29,7 +29,8 @@ def update_figure(selected_year):
 
     fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
                      size="pop", color="continent", hover_name="country",
-                     log_x=True, size_max=55)
+                     log_x=True, size_max=55,
+                     title="Strava Fanf")
 
     return fig
 
